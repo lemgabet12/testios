e7c7ff75443f814f19d6cf213a71ff0047f61482
lemgabet <aymenelghoul1993@gmail.com>
2024-12-09 11:07:27 +0100

    commmit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myApp',
-  webDir: 'www'
+  webDir: 'www',
+  ios: {
+    path: '/Users/ionic-cloud-team/builds/lemgabet12/testios', // Custom iOS build path
+  },
 };
 
 export default config;
