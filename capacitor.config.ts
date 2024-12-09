f1ca92271092462648fa84cee50b40c97c21f36a
lemgabet <aymenelghoul1993@gmail.com>
2024-12-09 11:22:13 +0100

    ffffffffffffffffffff

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myApp',
-  webDir: 'www'
+  webDir: 'www',
+  ios: {
+    // Custom iOS build path
+    path: 'ios', // This assumes you have the 'ios' folder in the root of your project
+  },
 };
 
 export default config;
